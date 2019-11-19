4bca86b733ddedb72640bee0a220c8f093af457b
209437 <209437@edu.p.lodz.pl>
2019-11-19 16:22:08 +0100

    Initial commit

---
+++ src/main/java/com/getEventsFromWEEIA/Controller.java
@@ -0,0 +1,18 @@
+package com.getEventsFromWEEIA;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Http request handler class
+ */
+@RestController
+public class Controller {
+
+    @RequestMapping("/getCalendar/{year}/{month}")
+    public String getDetailsAboutString(@PathVariable String year, @PathVariable String month) {
+        
+        return "response";
+    }
+}
---
+++ src/main/java/com/getEventsFromWEEIA/GetEventsFromWeeiaApplication.java
@@ -0,0 +1,13 @@
+package com.getEventsFromWEEIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetEventsFromWeeiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetEventsFromWeeiaApplication.class, args);
+	}
+
+}
